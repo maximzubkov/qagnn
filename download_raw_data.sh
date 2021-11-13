12dbb500919f6d33d4367291d6bd447cfda15128
maximzubkov <zubkov.md@phystech.edu>
2021-11-13 20:28:41 +0100

    Minor fix in scripts

--- download_raw_data.sh
+++ download_raw_data.sh
@@ -1,4 +1,4 @@
-download ConceptNet
+# download ConceptNet
 mkdir -p data/
 mkdir -p data/cpnet/
 wget -nc -P data/cpnet/ https://s3.amazonaws.com/conceptnet/downloads/2018/edges/conceptnet-assertions-5.6.0.csv.gz
