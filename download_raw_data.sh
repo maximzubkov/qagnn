5c89d5835d5cdaabe7eeecc35e3c6e2f2f4efb4c
maximzubkov <zubkov.md@phystech.edu>
2021-11-13 20:28:14 +0100

    Downloading script

--- download_raw_data.sh
+++ download_raw_data.sh
@@ -1,12 +1,12 @@
-# download ConceptNet
-#mkdir -p data/
-#mkdir -p data/cpnet/
-#wget -nc -P data/cpnet/ https://s3.amazonaws.com/conceptnet/downloads/2018/edges/conceptnet-assertions-5.6.0.csv.gz
-#cd data/cpnet/
-#yes n | gzip -d conceptnet-assertions-5.6.0.csv.gz
-## download ConceptNet entity embedding
-#wget https://csr.s3-us-west-1.amazonaws.com/tzw.ent.npy
-#cd ../../
+download ConceptNet
+mkdir -p data/
+mkdir -p data/cpnet/
+wget -nc -P data/cpnet/ https://s3.amazonaws.com/conceptnet/downloads/2018/edges/conceptnet-assertions-5.6.0.csv.gz
+cd data/cpnet/
+yes n | gzip -d conceptnet-assertions-5.6.0.csv.gz
+# download ConceptNet entity embedding
+wget https://csr.s3-us-west-1.amazonaws.com/tzw.ent.npy
+cd ../../
 
 
 
