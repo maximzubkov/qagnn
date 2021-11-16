eecf34e6d5809d8a8b791213a9b8cdc6da83bf4e
maximzubkov <zubkov.md@phystech.edu>
2021-11-16 13:49:06 +0100

    Update downloading script

--- download_preprocessed_data.sh
+++ download_preprocessed_data.sh
@@ -2,6 +2,6 @@
 
 mv data data_old
 
-wget https://nlp.stanford.edu/projects/myasu/QAGNN/data_preprocessed_release.zip
-unzip data_preprocessed_release.zip
-mv data_preprocessed_release  data
+mkdir data
+wget https://nlp.stanford.edu/projects/myasu/QAGNN/data_preprocessed_release.zip -P data
+cd data && unzip data_preprocessed_release.zip && mv data_preprocessed_release/* . && rm -rf data_preprocessed_release
